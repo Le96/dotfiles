a825d0eff4d11e117cc5868f64a0187575d3215d
Le96 <blitz1216@gmail.com>
2020-04-26 02:03:36 +0900

    Fix some broken scripts

--- preinstall.sh
+++ preinstall.sh
@@ -8,9 +8,9 @@ echo '[-] make directory for github.com.'
 cd $HOME
 mkdir -p ~/github.com/Le96
 echo '[-] make update script.'
-echo -e '#!/bin/sh\nfind . -type d -regextype egrep -regex \x27./[^/]+/[^/]+\x27 | \\\n  xargs -i sh -c \x27echo -n {}": "; cd {}; git pull && git submodule update --init --recursive\x27' > ~/github.com/_update.sh
-chmod +x ~/github.com/_update.sh
+ln -s ~/github.com/Le96/dotfiles/rcfiles/_update.sh ~/github.com/_update.sh
 echo '[-] install body of dotfiles.'
 git clone https://github.com/Le96/dotfiles.git ~/github.com/Le96/dotfiles
+
 echo '[+] launch install.sh.'
 sh ~/github.com/Le96/dotfiles/install.sh
